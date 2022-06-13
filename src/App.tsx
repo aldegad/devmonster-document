1c316ae8c248fb56d11e8206cfd81c940cc2a3c2
SyWolp <totqus130@gmail.com>
2022-06-13 17:46:43 +0900

    print

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./css/App.css";
+import Estimate from "./screen/estimate";
+
+
+export default function App() {
+  return (
+    <div className="printWrap">
+      <Estimate />
+    </div>
+  )
+}
---
+++ src/screen/estimate.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import Header from "../component/header";
+
+export default function Estimate() {
+    return (
+        <Header title="견적서"/>
+    )
+}
