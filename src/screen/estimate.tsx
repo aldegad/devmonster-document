6286ba6d5011a970bda19957aa9007a3038385d6
aldegad <aldegad@naver.com>
2022-06-16 21:15:18 +0900

    v1은 여까지하고... v2개선을 해야겠다

--- src/screen/estimate.tsx
+++ src/screen/estimate.tsx
@@ -23,61 +23,66 @@ export default function Estimate() {
     ]);
     
     const [secondLines, setSecondLines] = useState([
-        {secondTitle: "개발범위", content: "AOS Front-end, iOS Front-end"},
-        {secondTitle: "제작시스템", content: "Front-end: ionic angular"},
-        {secondTitle: "결제방식", content: "선금 30% / 중도금 30% / 잔금 40%"},
-        {secondTitle: "개발툴", content: "Visual Studio Code"}
+        {title: "개발범위", content: "AOS Front-end, iOS Front-end"},
+        {title: "제작시스템", content: "Front-end: ionic angular"},
+        {title: "결제방식", content: "선금 30% / 중도금 30% / 잔금 40%"},
+        {title: "개발툴", content: "Visual Studio Code"}
     ]);
 
     const [thirdLines, setThirdLines] = useState([
         {
             title : "1. 하이브리드앱 퍼블리싱 및 프론트 제작",
             content : [
-                {thirdTitle:"개발 설계 및 테마 제작", thirdPeriod: 15, thirdAmount: 2500000},
-                {thirdTitle:"네이티브 - 카메라", thirdPeriod: 1, thirdAmount: 400000},
-                {thirdTitle:"네이티브 - GPS", thirdPeriod: 2, thirdAmount: 800000},
-                {thirdTitle:"인증시스템(login/join/terms)", thirdPeriod: 4, thirdAmount: 1600000},
-                {thirdTitle:"인증시스템(find-id/find-password)", thirdPeriod: 1, thirdAmount: 400000},
-                {thirdTitle:"메인(swiper, list/card, tabs)", thirdPeriod: 2, thirdAmount: 800000},
-                {thirdTitle:"검색(search, list/store)", thirdPeriod: 1, thirdAmount: 400000},
-                {thirdTitle:"검색결과(search, list/card)", thirdPeriod: 0.5, thirdAmount: 200000},
-                {thirdTitle:"공지사항(segment, list/accordian, tabs)", thirdPeriod: 1.5, thirdAmount: 600000},
-                {thirdTitle:"강의신청(like, share, segment, rich-content, list/card)", thirdPeriod: 2, thirdAmount: 800000},
-                {thirdTitle:"강의결재(checkbox, list/edit, rich-content/edit, payment)", thirdPeriod: 3, thirdAmount: 1200000},
-                {thirdTitle:"마이페이지(rich-content, list/card, tabs)", thirdPeriod: 1, thirdAmount: 400000},
-                {thirdTitle:"회원정보수정(profile/edit, tabs)", thirdPeriod: 1.5, thirdAmount: 600000},
-                {thirdTitle:"찜한강의(checkbox, list/card, list/edit)", thirdPeriod: 2, thirdAmount: 800000},
-                {thirdTitle:"증명서 발급(list/card, tabs)", thirdPeriod: 1.5, thirdAmount: 600000},
-                {thirdTitle:"나의 강의실,메인(segment, video/custom-player, list/accordian, tabs, GPS)", thirdPeriod: 3, thirdAmount: 1200000},
-                {thirdTitle:"나의 강의실,공지사항(segment, list/accordian, tabs)", thirdPeriod: 1, thirdAmount: 400000},
-                {thirdTitle:"나의 강의실,강의(segment, video/custom-player, list, tabs, GPS)", thirdPeriod: 1.5, thirdAmount: 600000},
-                {thirdTitle:"나의 강의실,시험(segment, list, tabs)", thirdPeriod: 1, thirdAmount: 400000},
-                {thirdTitle:"나의 강의실,시험상세(segment, rich-content, tabs)", thirdPeriod: 1.5, thirdAmount: 600000},
-                {thirdTitle:"나의 강의실,시험도중(segment, rich-content/edit, tabs)", thirdPeriod: 2.5, thirdAmount: 1200000},
-                {thirdTitle:"나의 강의실,과제(segment, list, tabs)", thirdPeriod: 1, thirdAmount: 400000},
-                {thirdTitle:"나의 강의실,과제상세(segment, rich-content, tabs)", thirdPeriod: 1, thirdAmount: 400000},
-                {thirdTitle:"나의 강의실,과제제출(segment, rich-content/edit, tabs)", thirdPeriod: 1.5, thirdAmount: 600000},
+                {subTitle:"개발 설계 및 테마 제작", period: 15, amount: 2500000},
+                {subTitle:"네이티브 - 카메라", period: 1, amount: 400000},
+                {subTitle:"네이티브 - GPS", period: 2, amount: 800000},
+                {subTitle:"인증시스템(login/join/terms)", period: 4, amount: 1600000},
+                {subTitle:"인증시스템(find-id/find-password)", period: 1, amount: 400000},
+                {subTitle:"메인(swiper, list/card, tabs)", period: 2, amount: 800000},
+                {subTitle:"검색(search, list/store)", period: 1, amount: 400000},
+                {subTitle:"검색결과(search, list/card)", period: 0.5, amount: 200000},
+                {subTitle:"공지사항(segment, list/accordian, tabs)", period: 1.5, amount: 600000},
+                {subTitle:"강의신청(like, share, segment, rich-content, list/card)", period: 2, amount: 800000},
+                {subTitle:"강의결재(checkbox, list/edit, rich-content/edit, payment)", period: 3, amount: 1200000},
+                {subTitle:"마이페이지(rich-content, list/card, tabs)", period: 1, amount: 400000},
+                {subTitle:"회원정보수정(profile/edit, tabs)", period: 1.5, amount: 600000},
+                {subTitle:"찜한강의(checkbox, list/card, list/edit)", period: 2, amount: 800000},
+                {subTitle:"증명서 발급(list/card, tabs)", period: 1.5, amount: 600000},
+                {subTitle:"나의 강의실,메인(segment, video/custom-player, list/accordian, tabs, GPS)", period: 3, amount: 1200000},
+                {subTitle:"나의 강의실,공지사항(segment, list/accordian, tabs)", period: 1, amount: 400000},
+                {subTitle:"나의 강의실,강의(segment, video/custom-player, list, tabs, GPS)", period: 1.5, amount: 600000},
+                {subTitle:"나의 강의실,시험(segment, list, tabs)", period: 1, amount: 400000},
+                {subTitle:"나의 강의실,시험상세(segment, rich-content, tabs)", period: 1.5, amount: 600000},
+                {subTitle:"나의 강의실,시험도중(segment, rich-content/edit, tabs)", period: 2.5, amount: 1200000},
+                {subTitle:"나의 강의실,과제(segment, list, tabs)", period: 1, amount: 400000},
+                {subTitle:"나의 강의실,과제상세(segment, rich-content, tabs)", period: 1, amount: 400000},
+                {subTitle:"나의 강의실,과제제출(segment, rich-content/edit, tabs)", period: 1.5, amount: 600000},
             ]
         },
         {
             title: "2. 디버깅",
             content: [
-                {thirdTitle:"단위테스트", thirdPeriod: 30, thirdAmount: 5000000}
+                {subTitle:"단위테스트", period: 30, amount: 5000000}
             ]
         },
         {
             title: "3. 출시",
             content: [
-                {thirdTitle:"안드로이드 스토어 설정 및 배포", thirdPeriod: 7, thirdAmount: 1100000},
-                {thirdTitle:"아이폰 스토어 설정 및 배포", thirdPeriod: 14, thirdAmount: 2200000}
+                {subTitle:"안드로이드 스토어 설정 및 배포", period: 7, amount: 1100000},
+                {subTitle:"아이폰 스토어 설정 및 배포", period: 14, amount: 2200000}
             ]
         }
     ]);
 
-    const fourList = [
-        "기간은 Calendar Day를 기준으로 산정되었습니다.",
-        "보여주신 부분에서, 결재는 포함되고, 포인트는 빠진 것을 기준으로 견적이 작성되었습니다."
-    ]
+    const [fourList, setFourList] = useState([
+        {title: "기간은 Calendar Day를 기준으로 산정되었습니다."},
+        {title: "보여주신 부분에서, 결재는 포함되고, 포인트는 빠진 것을 기준으로 견적이 작성되었습니다."}
+    ])
+
+    const [total, setTotal] = useState([
+        {title: '실 작업기간', amount: 0},
+        {title: '합계', amount: 0},
+    ])
 
     const [correctBool, setCorrectBool] = useState(false);
     
@@ -86,10 +91,7 @@ export default function Estimate() {
     }
     const getElsx = (e:any) => {
         const fs = e.target.files;
-        console.log(fs);
         const f = fs[0];
-        console.log(f);
-
         var reader = new FileReader();
 
         reader.onload = function(e2:any) {
@@ -100,12 +102,48 @@ export default function Estimate() {
 
         workbook.SheetNames.forEach(function(sheetName:any, i:number) {
             // Here is your object
-            var XL_row_object = XLSX.utils.sheet_to_row_object_array(workbook.Sheets[sheetName]);
-            console.log(XL_row_object);
+            var data = XLSX.utils.sheet_to_row_object_array(workbook.Sheets[sheetName]);
             if(i === 0) {
-                setFirstLines(XL_row_object);
+                setFirstLines(data);
             }
-            console.log(firstLines);
+            else if(i === 1) {
+                setSecondLines(data);
+            }
+            else if(i === 2) {
+                let thirdData:any[] = [];
+                data.forEach((item:any) => {
+                    const { title, subTitle, period, amount } = item;
+                    const content = {
+                        subTitle,
+                        period: Number(period.replace(/,/g, '')),
+                        amount: Number(amount.replace(/,/g, ''))
+                    }
+                    if(title) {
+                        thirdData.push({
+                            title,
+                            content: [content]
+                        })
+                    }
+                    else {
+                        thirdData[thirdData.length-1].content.push(content)
+                    }
+                })
+                setThirdLines(thirdData);
+            }
+            else if(i === 3) {
+                setFourList(data);
+            }
+            else if(i === 4) {
+                console.log(data);
+                const parseData = data.map((row:any) => {
+                    return {
+                        title: row.title,
+                        amount: Number(row.amount.replace(/,/g,''))
+                    }
+                });
+                console.log(parseData);
+                setTotal(parseData);
+            }
         })
 
         };
@@ -118,7 +156,7 @@ export default function Estimate() {
     }
     return (
         <>
-            <input type="file" onChange={getElsx}></input>
+            <input className="not-print" type="file" onChange={getElsx}></input>
             <div className="estimate">
                 <Header title="견적서"/>
                 {/* <button style={{position:"absolute", top:'45mm', left:'73.3mm'}} onClick={correctButton}>{correctBool ? "수정 완료" : "수정 하기"}</button> */}
@@ -140,7 +178,7 @@ export default function Estimate() {
                         {secondLines.map((v,i)=>{
                             return (
                                 <div key={i} className="line">
-                                    <div key={v.secondTitle} className="firstLineTitle">{v.secondTitle}</div>
+                                    <div key={v.title} className="firstLineTitle">{v.title}</div>
                                     {correctBool ? <input type={"text"} className="firstLineContent" value={v.content} onChange={(e) => valueOnChange(e.target.value,i,secondLines,setSecondLines)} /> : <div key={v.content} className="firstLineContent">{v.content}</div>}
                                 </div>   
                             )
@@ -162,9 +200,9 @@ export default function Estimate() {
                                     {v.content.map((vv,i) => {
                                         return (
                                             <ul key={i} className="lineThreeList">
-                                                <li key={vv.thirdTitle} className="lineThreeFirst">{vv.thirdTitle}</li>
-                                                <li key={vv.thirdPeriod} className="lineThreeContent">{vv.thirdPeriod.toFixed(1)}</li>
-                                                <li key={vv.thirdAmount} className="lineThreeContent">{regex.replace.price(vv.thirdAmount)}</li>
+                                                <li key={vv.subTitle} className="lineThreeFirst">{vv.subTitle}</li>
+                                                <li key={vv.period} className="lineThreeContent">{vv.period.toFixed(1)}</li>
+                                                <li key={vv.amount} className="lineThreeContent">{regex.replace.price(vv.amount)}</li>
                                             </ul> 
                                         )
                                     })}
@@ -180,7 +218,7 @@ export default function Estimate() {
                             <ol>
                                 {fourList.map((v)=>{
                                     return (
-                                        <li>{v}</li>
+                                        <li>{v.title}</li>
                                     )
                                 })}
                             </ol>
@@ -191,14 +229,14 @@ export default function Estimate() {
                         <div className="fourLineContent">기간(일)</div>
                         <div className="fourLineContentResult">{
                             regex.replace.price(thirdLines.reduce((prev, thirdLine) => {
-                                return prev + thirdLine.content.reduce((_prev, item) => _prev + item.thirdPeriod, 0)
+                                return prev + thirdLine.content.reduce((_prev, item) => _prev + item.period, 0)
                             }, 0))
                         }</div>
                     </div>
                     <div className="line">
                         <div className="fourLineTitle">실 작업기간</div>
                         <div className="fourLineContent">기간(일)</div>
-                        <div className="fourLineContentResult">90</div>
+                        <div className="fourLineContentResult">{total[0].amount}</div>
                     </div>
                     <div className="line">
                         <div className="fourLineTitle">항목별 견적</div>
@@ -210,17 +248,17 @@ export default function Estimate() {
                         <ul className="fourLineContentResult">
                             <li className="fourCalculation">{
                                 regex.replace.price(thirdLines.reduce((prev, thirdLine) => {
-                                    return prev + thirdLine.content.reduce((_prev, item) => _prev + item.thirdAmount, 0)
+                                    return prev + thirdLine.content.reduce((_prev, item) => _prev + item.amount, 0)
                                 }, 0))
                             }</li>
                             <li className="fourCalculation">{
                                 regex.replace.price(thirdLines.reduce((prev, thirdLine) => {
-                                    return prev + thirdLine.content.reduce((_prev, item) => _prev + item.thirdAmount, 0)
+                                    return prev + thirdLine.content.reduce((_prev, item) => _prev + item.amount, 0)
                                 }, 0)*0.1)
                             }</li>
                             <li className="fourCalculation">{
                                 regex.replace.price((thirdLines.reduce((prev, thirdLine) => {
-                                    return prev + thirdLine.content.reduce((_prev, item) => _prev + item.thirdAmount, 0)
+                                    return prev + thirdLine.content.reduce((_prev, item) => _prev + item.amount, 0)
                                 }, 0)*1.1).toFixed(0))
                             }</li>
                         </ul>
@@ -233,9 +271,9 @@ export default function Estimate() {
                             <li className="fourCalculation">총계(부가세포함)</li>
                         </ul>
                         <ul className="fourLineContentResult">
-                            <li className="fourCalculation">20,000,000</li>
-                            <li className="fourCalculation">2,000,000</li>
-                            <li className="fourCalculation">22,000,000</li>
+                            <li className="fourCalculation">{regex.replace.price(total[1].amount)}</li>
+                            <li className="fourCalculation">{regex.replace.price(total[1].amount*0.1)}</li>
+                            <li className="fourCalculation">{regex.replace.price((total[1].amount*1.1).toFixed(0))}</li>
                         </ul>
                     </div>        
                 </div>
