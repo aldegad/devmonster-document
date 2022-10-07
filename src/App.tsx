c00ab226b18add4701c9c5340280913fcf79edf9
aldegad <aldegad@naver.com>
2022-10-07 20:15:59 +0900

    add no print

--- src/App.tsx
+++ src/App.tsx
@@ -9,8 +9,9 @@ const button = (e:any) => {
   /* e.target.style.display = "none";
   window.print();
   e.target.style.display = "inline-block"; */
-  alert('open!');
-  window.open('com.cntti.cntti://expo-development-client/?url=http%3A%2F%2F172.30.1.19%3A8081');
+  
+  //window.open('com.cntti.cntti://expo-development-client/?url=http%3A%2F%2F172.30.1.19%3A8081');
+  location.href = 'com.cntti.cntti://expo-development-client/?url=http%3A%2F%2F172.30.1.19%3A8081';
 }
 
 
@@ -30,7 +31,7 @@ export default function App() {
         <ContractTwo />
       </div> */}
       <div>
-      <button onClick={button}>프린트 버튼</button>
+      <button className="no-print" onClick={button}>프린트 버튼</button>
       </div>
     </div>
   )
